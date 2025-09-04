cf0d950070205d76bbd88e1dd62a5026beb05bc9
igortteixeira <igorteixeira2305@gmail.com>
2025-09-04 21:00:48 +0100

    Overall Web Chat Deletion

--- Web_chat/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-import home.views as views
-
-
-urlpatterns = [
-path("",views.home_view,name='home_page'),
-path("search_users/",views.search_users_view,name='search_users_page'),
-#path("search_users/<str:parameter_search_string>",views.search_users_view,name='search_users_page'),
-]
